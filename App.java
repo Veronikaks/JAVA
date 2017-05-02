86a966d32d9d1fee308732ef0ceb0bef732faa03
Veronika <ronika_ks@mail.ru>
2017-05-02 19:53:40 +0300

    Initial commit

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {    //имя файла должно быть таким же как и имя класса
+	
+	public static void main(String[] args){      //стандартный шаблон для мэйн метода
+		System.out.println("Hello, world!");
+	}
+
+	
+}
